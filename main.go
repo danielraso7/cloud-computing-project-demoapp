f465757dfffd7f77089764cf23357b38d6b582f0
unknown <eva.fitzinger@gmail.com>
2023-01-18 00:27:26 +0100

    test

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func HttpRequest(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, you've requested:  %s\n", r.URL.Path)
+	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 }
 
 func main() {
